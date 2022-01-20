d794eba21b245ff6182e506cfd0ae171076c8f10
Pier <p.delaunay1@gmail.com>
2022-01-20 20:29:52 +0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -44,17 +44,42 @@ hbs.registerPartials( partialsPath );
 app.use( express.static( publishDirectoryPath ) );
 
 
-app.use(bodyParser.json()); 
+app.get('/', (req, res) => {
+  	res.render( "index", {
+		title: "Run node App",
+	} );
+  });
+
+  
+app.get( "/about", function ( req, res ) {
+	res.render( "about" );
+} );
+// app.use('/about', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+//   router.get('/another', (req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('<h1>Hello another Express.js!</h1>');
+//     res.end();
+//   });
+
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
+app.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// router.post('/another', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-router.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+//404 page - A mettre a la fin des app.get !
+app.get('*',(req, res) => {
+    res.send(`
+        My 404 page
+        <br/>
+        <a href="/">Come back home</a>`
+    )
+})
 
 // app.get( "/", function ( req, res ) {
 // 	res.render( "index", {
@@ -62,7 +87,6 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 // 	} );
 // } );
 
-// app.get( "/about", function ( req, res ) {
-// 	res.render( "about" );
-// } );
+// router.post('/about', (req, res) => res.json({ postBody: req.body }));
+
 
