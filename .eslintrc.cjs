1b9364496574538e1f07179a6b32843dddecf101
Pier <p.delaunay1@gmail.com>
2022-01-20 11:36:14 +0400

    ajout express js

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        indent: [ "error", "tab" ],
+        "linebreak-style": [ "error", "unix" ],
+        quotes: [ "error", "double" ],
+        semi: [ "error", "always" ],
+        "array-bracket-spacing": [ "error", "always" ],
+        "object-curly-spacing": [ "error", "always" ],
+        "space-in-parens": [ "error", "always" ]
+    }
+}
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+import chalk from 'chalk';
+
+
+// console.log(chalk.inverse.green('Starting app in dev mode...')); // eslint-disable-line no-console
+// console.log(chalk.inverse.red('Stopping app in dev mode...'));      
+
+
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World et le reste')
+})
+
+app.get('/about', function (req, res) {
+    res.send('A propos de moi')
+  })
+
+app.listen(3000)
+console.log(chalk.inverse.green('Starting serveur on port 3000...')); // eslint-disable-line no-console
