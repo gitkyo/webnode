e06b81a3b3c8215289db3f32c11978f931c6c368
Pier <p.delaunay1@gmail.com>
2022-01-20 13:47:22 +0400

    add es lint conf

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+import path from "path";
 
-import chalk from 'chalk';
-import express from 'express'
-import hbs from 'hbs';
+import chalk from "chalk";
+import express from "express";
+import hbs from "hbs";
 
 // exemple d'import via requiere
 // const express = require('express')
@@ -15,31 +15,31 @@ import hbs from 'hbs';
 //set var to get path directory
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const publishDirectoryPath = path.join(__dirname, '/public') 
-const viewsPath = path.join(__dirname, '/templates/views')
-const partialsPath = path.join(__dirname, 'templates/partials')
+const __filename = fileURLToPath( import.meta.url );
+const __dirname = dirname( __filename );
+const publishDirectoryPath = path.join( __dirname, "/public" ); 
+const viewsPath = path.join( __dirname, "/templates/views" );
+const partialsPath = path.join( __dirname, "templates/partials" );
 
 
-const app = express()
+const app = express();
 
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-app.use(express.static(publishDirectoryPath))
+app.set( "view engine", "hbs" );
+app.set( "views", viewsPath );
+hbs.registerPartials( partialsPath );
+app.use( express.static( publishDirectoryPath ) );
 
 
-app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'Run node App',
-  })
-})
+app.get( "/", function ( req, res ) {
+	res.render( "index", {
+		title: "Run node App",
+	} );
+} );
 
-app.get('/about', function (req, res) {
-    res.render('about')
-  })
+app.get( "/about", function ( req, res ) {
+	res.render( "about" );
+} );
 
-app.listen(3000)
-console.log(chalk.inverse.green('Starting serveur on port 3000...')); // eslint-disable-line no-console
+app.listen( 3000 );
+console.log( chalk.inverse.green( "Starting serveur on port 3000..." ) ); // eslint-disable-line no-console
