913f1e84e30d246dea1468a5bbc8e5d68eb65013
Pier <p.delaunay1@gmail.com>
2022-01-20 15:01:24 +0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -3,6 +3,9 @@ import { dirname } from "path"
 import path from "path";
 
 
+
+
+
 import express from "express";
 import hbs from "hbs";
 
@@ -15,8 +18,9 @@ import hbs from "hbs";
 //set var to get path directory
 
 
-const __filename = fileURLToPath( import.meta.url );
-const __dirname = dirname( __filename );
+// const __filename = fileURLToPath( import.meta.url );
+// const __dirname = dirname( __filename );
+const __dirname = path.resolve();
 const publishDirectoryPath = path.join( __dirname, "/public" ); 
 const viewsPath = path.join( __dirname, "/templates/views" );
 const partialsPath = path.join( __dirname, "templates/partials" );
